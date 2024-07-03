a5de248fb2e0d31601e1cb36d7e01b9e74ca3243
geonugang <cooper091677@gmail.com>
2024-07-03 09:57:53 +0900

    trying comment

--- src/main/java/Project/PetWalk/dto/UserDto.java
+++ src/main/java/Project/PetWalk/dto/UserDto.java
@@ -1,24 +1,24 @@
-//package Project.PetWalk.dto;
-//
-//import lombok.*;
-//
-//import java.time.LocalDateTime;
-//
-//@Setter
-//@Getter
-//@Data
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class UserDto {
-//    // Getters and Setters
-//    private Long idx;
-//    private String username;
-//    private String email;
-//    private String phoneNumber;
-//    private String address;
-//    private LocalDateTime createdAt;
-//    private LocalDateTime updatedAt;
-//    private int point;
-//
-//}
+package Project.PetWalk.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    // Getters and Setters
+    private Long idx;
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private int point;
+
+}
